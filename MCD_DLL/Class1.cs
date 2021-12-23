369a420a03161890261caf2510d1eeb9dbd69050
jackup <yk.58@hotmail.com>
2021-12-23 18:02:13 +0300

    Add project files.

---
+++ MCD_DLL/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MCD_DLL
+{
+    public class Class1
+    {
+        public decimal Topla(decimal deger1, decimal deger2)
+        {
+            return deger1 + deger2;
+        }
+
+        public decimal Cikar(decimal deger1, decimal deger2)
+        {
+            return deger1 - deger2;
+        }
+        public decimal Carp(decimal deger1, decimal deger2)
+        {
+            return deger1 * deger2;
+        }
+        public decimal Bol(decimal deger1, decimal deger2)
+        {
+            if (deger2 == 2)
+                deger2 = 1;
+            return deger1 / deger2;
+        }
+    }
+}
---
+++ MCD_DLLCagirma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_DLLCagirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MCD_DLL.Class1 c1 = new MCD_DLL.Class1();
+            decimal a = c1.Carp(15, 8);
+            Console.WriteLine(a);
+        }
+    }
+}
